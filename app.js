a40677d4b7cb2c546da3f70b72ad1ec112ab1b4d
seron-ux <abyseronkirui@gmail.com>
2020-12-12 11:46:22 +0300

    declared the variables for the days of the week and the names

---
+++ app.js
@@ -0,0 +1,23 @@
+//declaring variables
+
+var daysOfWeek=[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+
+var femaleNames=[
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama",
+];
+
+
