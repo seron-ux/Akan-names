055e10aad29e92566d382931195ba653aa18f60e
seron-ux <abyseronkirui@gmail.com>
2020-12-12 11:56:26 +0300

    called function by listing each variable to get each element by id

--- app.js
+++ app.js
@@ -21,3 +21,13 @@ var femaleNames=[
 ];
 
 
+// calling function
+function getName(){
+    var year=parseInt(document.getElementById("year").value);
+    var month =parseInt(document.getElementById("month").value);
+    var day=parseInt(document.getElementById("day").value);
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
+
+    
+}
